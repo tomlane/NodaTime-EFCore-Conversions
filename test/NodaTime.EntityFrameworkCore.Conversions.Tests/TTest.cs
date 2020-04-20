b98d39a43fe029f65e2c47952644919d835ab8ef
Tom Lane <tom@tomlane.codes>
2020-04-20 18:21:56 +0100

    Setup test project

---
+++ test/NodaTime.EntityFrameworkCore.Conversions.Tests/TTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace NodaTime.EntityFrameworkCore.Conversions.Tests
+{
+    public class TTest
+    {
+        [Fact]
+        public void given_true_should_be_true()
+        {
+            Assert.True(true);
+        }
+    }
+}
